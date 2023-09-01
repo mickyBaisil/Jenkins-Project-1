45172d91fa1cc032867c745bbf02ce3f9121e652
mickyBaisil <120027056+mickyBaisil@users.noreply.github.com>
2023-09-01 15:14:22 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#installing Java
+sudo apt update
+sudo apt install -y openjdk-11-jre
+
 # Download Jenkins repository key
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
